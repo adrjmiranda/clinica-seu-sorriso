1727a07bd4cd8ffd85c2aab43eab21a15409be12
Adriano Miranda <adrjmiranda@gmail.com>
2025-08-14 21:27:44 -0300

    chore: created topbar component

--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -1,3 +1,66 @@
+import { BsGeoFill } from 'react-icons/bs';
+import {
+	FaFacebookF,
+	FaInstagram,
+	FaLinkedinIn,
+	FaTwitter,
+} from 'react-icons/fa';
+import { MdMail, MdPhone } from 'react-icons/md';
+
 export default function Topbar() {
-	return <div>Topbar</div>;
+	return (
+		<div className="fixed inset-0 hidden h-12 w-full items-center bg-white text-gray-600 md:flex">
+			<div className="horizontal-wrapper flex w-full justify-between">
+				<address className="flex items-center gap-4">
+					<p className="flex items-center gap-1">
+						<MdPhone />
+						<span>Fale conosco +55 73 3525-6518</span>
+					</p>
+					<p className="flex items-center gap-1">
+						<MdMail />
+						<a href="mailto:seusorriso@contato.com">seusorriso@contato.com</a>
+					</p>
+					<p className="flex items-center gap-1">
+						<BsGeoFill />
+						<span>Nossa localização</span>
+					</p>
+				</address>
+
+				<div className="flex items-center gap-4">
+					<a
+						href="#"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="hover:text-sky transition-colors duration-300 ease-in-out"
+					>
+						<FaFacebookF />
+					</a>
+					<a
+						href="#"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="hover:text-sky transition-colors duration-300 ease-in-out"
+					>
+						<FaLinkedinIn />
+					</a>
+					<a
+						href="#"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="hover:text-sky transition-colors duration-300 ease-in-out"
+					>
+						<FaInstagram />
+					</a>
+					<a
+						href="#"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="hover:text-sky transition-colors duration-300 ease-in-out"
+					>
+						<FaTwitter />
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -2,9 +2,10 @@ export default function HeroSection() {
 	return (
 		<header
 			id="home"
-			className="vertical-wrapper"
+			className="vertical-wrapper relative"
 		>
-			<h2>HeroSection</h2>
+			<picture className="absolute inset-0 h-full w-full"></picture>
+			<div></div>
 		</header>
 	);
 }
